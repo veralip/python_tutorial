f6cede70663b3d076fdc2f93d26d69855a455ea3
veralip <p.velascoperez@outlook.com>
2022-09-16 22:02:51 +0200

    first commit

---
+++ bucles/blucle_6.py
@@ -0,0 +1,15 @@
+"""
+Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo 
+como el de más abajo, de altura el número introducido.
+
+*
+**
+***
+****
+*****
+"""
+n=int(input("escribe un numero"))
+for i in range(n):
+    print("*"*(i+1))
+    
+
---
+++ bucles/blucle_7.py
@@ -0,0 +1,7 @@
+"""
+Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+"""
+for i in range(1,11):
+    for j in range(1,11):
+        print(i*j, end=" ")
+    print("\n")
